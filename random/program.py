f6b583f2e09fbae6a9cf860ee7be8a5937bf0b75
JIMMII88\ADMIN <thanhntt89bk@gmail.com>
2021-07-15 03:32:50 +0700

    Revert "Init projects"
    This reverts commit 25cac39f89c5f98fc44aa14c8b7d997051edc12f.

--- random/program.py
+++
@@ -1,8 +0,0 @@
-from password_utils import passwordUtils
-
-def main():
-    pass_generator = passwordUtils()
-    print(f'Passwords: {pass_generator.passwordGenerator(16)}')
-
-if __name__ =="__main__":
-    main()

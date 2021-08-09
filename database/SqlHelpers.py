074018c523b2958092dd22e797470e5e2d578284
jimii88 <nguyen.tat.thanh@miyatsu.vn>
2021-08-09 11:53:14 +0700

    no message

--- database/SqlHelpers.py
+++ database/SqlHelpers.py
@@ -1,23 +1,28 @@
 ﻿import pyodbc as odbc
 
-class SqlHelpers():
+class SqlHelpers(object):
     #Default connection information
     SERVER = 'WINSERVER2016'
-    USER_NAME='wiiAdmin'
-    PASSWORDS='W_iiAdmin00000'
-    DATABASE ='Wii'
+    USER_NAME = 'wiiAdmin'
+    PASSWORDS = 'W_iiAdmin00000'
+    DATABASE = 'Wii'
     COMMAND_TIMEOUT = 5000
 
     connection_string = 'DRIVER={SQL Server};SERVER='+SERVER+';DATABASE='+DATABASE+';UID='+USER_NAME+';PWD='+ PASSWORDS
     
     @staticmethod
-    def test_connection():        
+    def GetConnectionString():
+        SqlHelpers.connection_string = 'DRIVER={SQL Server};SERVER='+SqlHelpers.SERVER+';DATABASE='+SqlHelpers.DATABASE+';UID='+SqlHelpers.USER_NAME+';PWD='+ SqlHelpers.PASSWORDS 
+        return SqlHelpers.connection_string
+
+    @staticmethod
+    def test_connection(connection_string):        
         try:            
-            con = odbc.connect(SqlHelpers.connection_string)
+            con = odbc.connect(connection_string)
             con.timeout = SqlHelpers.COMMAND_TIMEOUT
-            cur = con.cursor()            
-            query ="SELECT @@version;"
-            cur.execute(query)            
+            cur = con.cursor()
+            cur.open()  
+            cur.closest()
             return True
         except:
             return False
@@ -27,26 +32,32 @@ class SqlHelpers():
     def ExecuteDataset(connection_string,query):
         con = odbc.connect(connection_string)
         con.timeout = SqlHelpers.COMMAND_TIMEOUT
-        cur = con.cursor()
-        cur.execute(query)  
-        return cur.fetchall() 
+        cur = con.cursor()        
+        cur.execute(query) 
+        dat_row = cur.fetchall()
+        columns = [cols[0] for cols in cur.description]
+        return columns,dat_row
 
              
 
 def main():
     SqlHelpers.SERVER ='WINSERVER2016'
-    SqlHelpers.DATABASE ='WiiTmp'
+    SqlHelpers.DATABASE ='Wii'
     SqlHelpers.PASSWORDS ='W_iiAdmin00000' 
     SqlHelpers.USER_NAME ='wiiAdmin'
 
-    #print('Sql connection string :'+ SqlHelpers.connection_string)
+    connection_string = SqlHelpers.GetConnectionString()
 
-    #print('Test connected:' + str(SqlHelpers.test_connection()))
+    print('Sql connection string :'+ connection_string)
+
+    print('Test connected:' + str(SqlHelpers.test_connection(connection_string)))
 
     #query = 'select [利用者ID],[権限グループ],[利用者名] from  [Wii].[dbo].[Fes利用者]'
     query = 'EXEC [dbo].[select_test]'
-    user_group_dataset = SqlHelpers.ExecuteDataset(SqlHelpers.connection_string, query)
+    columns , user_group_dataset = SqlHelpers.ExecuteDataset(connection_string, query)
+
     print('User_groups:\n')
+    print(columns)
     #print(user_group)
     for ug in user_group_dataset:
         print(ug)

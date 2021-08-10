6a8022c9620fa1082535e380c7fe9bdb7ace81e3
jimii88 <nguyen.tat.thanh@miyatsu.vn>
2021-08-11 02:13:23 +0700

    Init rest api

---
+++ api/sever.py
@@ -0,0 +1,25 @@
+﻿from flask import Flask
+import pandas as pd
+
+
+class Server():
+
+    app = Flask(__name__)    
+    app.config["DEBUG"] = True
+
+    def __init__(self):
+        print('this main server class')
+
+    @app.route('/', methods=['GET'])
+    def getUsers():
+        return tuple([{"id": 1, "name": "Company One"}, {"id": 2, "name": "Company Two"}])
+
+def main():
+    #print('this main function')
+    HOST ='localhost'
+    PORT = 8888
+    server = Server()    
+    server.app.run(host=HOST, port=PORT)
+
+if __name__ == '__main__':
+    main()

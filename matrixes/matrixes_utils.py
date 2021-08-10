259ae0968976aac62738bc9bd9798b3b71aac084
JIMMII88\ADMIN <thanhntt89bk@gmail.com>
2021-08-11 00:16:44 +0700

    no message

--- matrixes/matrixes_utils.py
+++ matrixes/matrixes_utils.py
@@ -16,6 +16,12 @@ class matrixes():
     def multiplyTowMatrixUsingNetListComprehension(self,matrixX,matrixY):
         return [[sum(a*b for a,b in zip(x_row,y_col)) for y_col in zip(*matrixY)] for x_row in matrixX]
     
+    def matrixJoinColumn(self,matrixX, matrixY):
+        return np.dot(matrixX,matrixY).shape()
+
+    def matrixMultiplyNp(self, matrixX, matrixY):
+        return np.matmul(matrixX,matrixY).shape()
+
 def main():
     _matrix = matrixes()
 
@@ -27,8 +33,15 @@ def main():
     print(f'Combine columbers:{_matrix.combineColumns(matrix)}')
     print(f'Convert convert_to_array:{_matrix.convert_to_array(matrix)}') 
 
-    matrixX = [[12,7,5],[6,3,5]]
-    matrixY =[[3,6,9],[7,8,2]]
+    matrixX = [[12,7,5],
+               [6,3,5],
+               [8,34,24]]
+    matrixY =[[3,6,9],
+              [7,8,2],
+              [6,8,3]]
+
+    #join
+    #print(f'NP: X*Y = {_matrix.matrixJoinColumn(matrixX,matrixY)}')
 
     print(f'MatrixX:{matrixX}\nmatrixY:{matrixY}')
     print(f'MatrixX + MatrixY = {_matrix.addTowMatrix(matrixX,matrixY)}')

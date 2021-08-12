7bbe1749b7bd4b37aa36ec68228108ba29344497
JIMMII88\ADMIN <thanhntt89bk@gmail.com>
2021-08-12 08:42:24 +0700

    no message

--- api/sever.py
+++ api/sever.py
@@ -1,13 +1,14 @@
 ﻿from flask import Flask
 import pandas as pd
 from modules.user import user
-
+from modules.auth import auth
 class Server():
 
+    API_VER = '/api/v2'
     app = Flask(__name__)    
     #app.config["DEBUG"] = True    
-    app.register_blueprint(user.user_bp, url_prefix='/user/')
-  
+    app.register_blueprint(user.user_bp, url_prefix = f'{API_VER}/user/')
+    app.register_blueprint(auth.auth_bp, url_prefix = f'{API_VER}/auth/')
 
 
 def main():

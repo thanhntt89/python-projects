fd744391f5012ee7bfa291b6b25937c2b38b18eb
jimii88 <nguyen.tat.thanh@miyatsu.vn>
2021-08-19 11:16:39 +0700

    remove code not use

--- api/sever.py
+++ api/sever.py
@@ -1,19 +1,21 @@
 ﻿from flask import Flask
-import pandas as pd
 from modules.user import user
 from modules.auth import auth
+from modules.member import member
 class Server():
 
     API_VER = '/api/v2'
     app = Flask(__name__)    
     #app.config["DEBUG"] = True    
     #app.register_blueprint(auth.authority_bp, url_prefix='/api/v1/login/')   
-  
-    app.register_blueprint(user.user_bp, url_prefix = f'{API_VER}/user/')
+
     app.register_blueprint(auth.auth_bp, url_prefix = f'{API_VER}/auth/')
+    app.register_blueprint(user.user_bp, url_prefix = f'{API_VER}/user/')   
+    app.register_blueprint(member.member_bp, url_prefix = f'{API_VER}/member/')
 
 
-def main():
+def main():    
+
     #print('this main function')
     HOST ='localhost'
     PORT = 8888

69217288096d63973ba0775eda4a6636b3b9ce59
jimii88 <nguyen.tat.thanh@miyatsu.vn>
2021-08-19 11:25:45 +0700

    update required token

--- api/modules/user.py
+++ api/modules/user.py
@@ -1,5 +1,6 @@
 ﻿from flask import Blueprint, jsonify, request
 from modules.SqlHelpers import SqlHelpers
+from modules.getconnection import GetConnection
 from modules.auth import auth
 class user():
 
@@ -7,7 +8,6 @@ class user():
     user_bp = Blueprint('user',__name__)
     
     def __init__(self):
-        self.user_bp = Blueprint('user',__name__)
         print('this user class')
     
     @user_bp.route('/')
@@ -18,12 +18,12 @@ class user():
     #Check token_required
     @auth.token_required
     def users():
-        #key = request.headers.get('key')
-        #print(f'key: {key}')
-        #key_body = request.form.get('key_body')
-        #print(f'key_body: {key_body}')
-        query = f'SELECT [利用者ID] as user_id ,[権限グループ] as user_name ,[利用者名] as role  FROM [Wii].[dbo].[Fes利用者]'        
-        df = SqlHelpers.ExecuteDataFrame(SqlHelpers.GetConnectionString(), query)
+        key = request.headers.get('key')
+        print(f'key: {key}')
+        key_body = request.form.get('key_body')
+        print(f'key_body: {key_body}')
+        query = 'SELECT [利用者ID] as user_id ,[権限グループ] as user_name ,[利用者名] as role  FROM [Wii].[dbo].[Fes利用者]'        
+        df = SqlHelpers.ExecuteDataFrame(GetConnection.sqlconnectiostring, query)
         return df.to_json(orient="records")
 
 def main():

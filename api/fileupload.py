b89f5f86fd974d2b0fb0393264c26dda8f6ba82a
jimii88 <nguyen.tat.thanh@miyatsu.vn>
2021-08-19 13:53:18 +0700

    init upload file

---
+++ api/fileupload.py
@@ -0,0 +1,17 @@
+﻿from flask import Blueprint, jsonify, request
+import os
+class uploadFile(object):
+    FOLDER_UPLOAD_NAME = 'upload'
+    ROOT_PATH = os.path.join(os.path.dirname(__file__),FOLDER_UPLOAD_NAME)
+    EXTENSIONS ={'txt', 'csv','xls','xlsx','jpg','jpeg','mp4','mp3'}
+    upload_bp = Blueprint('upload')
+
+    @upload_bp.route('/', methods = ['POST','PUT'])
+    def __init__():
+
+        pass
+
+    def uploadFile():
+        client_path = request.form.get('file_path_name')
+
+        pass
